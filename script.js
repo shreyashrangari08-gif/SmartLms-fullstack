561586329d5ee1c6ce4ae09f40947928b37ab793
shreyashrangari08-gif <shreyashrangari08@gmail.com>
2026-05-26 19:51:25 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js - Button interaction ke liye
+function enroll(courseName) {
+    alert("Great! Aapne " + courseName + " course ke liye enroll kar liya hai.");
+    console.log("User enrolled in: " + courseName);
+    
+    // Yahan hum baad mein backend se database connection karenge
+}
